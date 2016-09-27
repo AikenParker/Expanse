403aa2086b0a20d33ccfe39b47ccc8dc3406a5a0
Aiken Parker <aikenparker95@gmail.com>
2016-09-28 06:44:48 +1000

    Moved stuff around and added utility folder

---
+++ Utility/StreamingAssetsUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Expanse
+{
+    public static class StreamingAssetsUtil
+    {
+        public static string GetFullStreamingAssetFilepath(string filepath)
+        {
+            return string.Concat(Application.streamingAssetsPath, filepath);
+        }
+    }
+}
