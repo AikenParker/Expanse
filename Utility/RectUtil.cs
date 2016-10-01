1e64de56283e71c1b3567ec1b03eef9e6f9b42c0
Aiken Parker <aikenparker95@gmail.com>
2016-10-02 08:05:01 +1100

    Fixed directory issues

---
+++ Utility/RectUtil.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Expanse;
+
+namespace Expanse
+{
+    public static class RectUtil
+    {
+        public static Rect AddSize(this Rect source, float addWidth, float addHeight)
+        {
+            Rect newRect = new Rect(source);
+            newRect.width += addWidth;
+            newRect.height += addHeight;
+            return newRect;
+        }
+
+        public static Rect SetSize(this Rect source, float width, float height)
+        {
+            Rect newRect = new Rect(source);
+            newRect.width = width;
+            newRect.height = height;
+            return newRect;
+        }
+
+        public static Rect AddPosition(this Rect source, float addX, float addY)
+        {
+            Rect newRect = new Rect(source);
+            newRect.x += addX;
+            newRect.y += addY;
+            return newRect;
+        }
+
+        public static Rect SetPosition(this Rect source, float x, float y)
+        {
+            Rect newRect = new Rect(source);
+            newRect.x = x;
+            newRect.y = y;
+            return newRect;
+        }
+
+        public static Rect SplitWidth(this Rect source, int index, int count, float spacing = 0f)
+        {
+            if (source.width == 0)
+                return source;
+
+            float totalSpacing = spacing * (count - 1);
+            float workableWidth = source.width - totalSpacing;
+            float singleWidth = workableWidth / count;
+            float step = singleWidth + spacing;
+            float startX = (index - 1) * step;
+
+            Rect newRect = new Rect(source);
+            newRect.x += startX;
+            newRect.width = step;
+
+            return newRect;
+        }
+
+        //public Rect SplitWidth(this Rect source, int index, float[] definitions, float spacing = 0f)
+        //{
+
+        //}
+    }
+}
---
+++ Utility/VectorUtil.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Expanse;
+
+public static class VectorUtil
+{
+    public static Vector3 Create(float value, Vector3Dim dims = Vector3Dim.XYZ)
+    {
+        Vector3 vec = Vector3.zero;
+        if (dims.IsFlagSet(Vector3Dim.X))
+            vec.x = value;
+        if (dims.IsFlagSet(Vector3Dim.Y))
+            vec.y = value;
+        if (dims.IsFlagSet(Vector3Dim.Z))
+            vec.z = value;
+        return vec;
+    }
+
+    /// <summary>
+    /// Gets a point dist units from A, in direction B.
+    /// </summary>
+    public static Vector3 GetPointAtDistAlongLine(Vector3 pointA, Vector3 pointB, float dist, bool fromA)
+    {
+        float vx = pointB.x - pointA.x;
+        float vy = pointB.y - pointA.y;
+        float vz = pointB.z - pointA.z;
+
+        float mag = Mathf.Sqrt(vx * vx + vy * vy + vz * vz); // length
+
+        vx /= mag;
+        vy /= mag;
+        vz /= mag;
+
+        Vector3 point = new Vector3();
+        if (fromA)
+        {
+            point.x = pointA.x + vx * (mag + dist);
+            point.y = pointA.y + vy * (mag + dist);
+            point.z = pointA.z + vz * (mag + dist);
+        }
+        else
+        {
+            point.x = pointB.x + vx * (mag + dist);
+            point.y = pointB.y + vy * (mag + dist);
+            point.z = pointB.z + vz * (mag + dist);
+        }
+        return point;
+    }
+
+    /// <summary>
+    /// Determines the angle between 2 Vector3s.
+    /// </summary>
+    public static float AngleBetweenTwoPoints(Vector3 vecA, Vector3 vecB)
+    {
+        return Mathf.Acos(Vector3.Dot(vecA, vecB) / (vecA.magnitude * vecB.magnitude));
+    }
+
+    /// <summary>
+    /// Determines the angle between 3 Vector3s.
+    /// </summary>
+    public static float AngleBetweenThreePoints(Vector3 vecA, Vector3 vecB, Vector3 vecC)
+    {
+        return AngleBetweenTwoPoints(vecA - vecC, vecB - vecC);
+    }
+}
+
+[Flags]
+public enum Vector3Dim
+{
+    NONE = 0,
+    X = 1 << 0,
+    Y = 1 << 1,
+    Z = 1 << 2,
+    XZ = X | Z,
+    XY = X | Y,
+    XYZ = X | Y | Z
+}
