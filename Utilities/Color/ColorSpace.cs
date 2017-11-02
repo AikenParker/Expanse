1cfee73c0ad82f1addec484dd6347fe41d5778d0
Aiken Parker <aikenparker95@gmail.com>
2017-11-02 22:01:00 +1100

    Work on color space conversions

---
+++ Utilities/Color/ColorSpace.cs
@@ -0,0 +1,396 @@
+﻿#region USINGS
+
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using Expanse;
+using Expanse.Misc;
+using Expanse.Motion;
+using Expanse.Utilities;
+using Expanse.Extensions;
+
+#endregion
+
+namespace Expanse.Utilities
+{
+    // References:
+    // https://www.codeproject.com/Articles/19045/Manipulating-colors-in-NET-Part
+    // https://github.com/THEjoezack/ColorMine
+    // http://www.deathbysoftware.com/colors/index.html
+
+    public static class ColorSpace
+    {
+        public interface IColorSpace
+        {
+            RGB ToRGB();
+            void FromRGB(RGB rgb);
+        }
+
+        public struct RGB : IColorSpace
+        {
+            public double r;
+            public double g;
+            public double b;
+
+            public RGB(double r, double g, double b)
+            {
+                this.r = r;
+                this.g = g;
+                this.b = b;
+            }
+            
+            public RGB(Color color)
+            {
+                this.r = color.r;
+                this.g = color.g;
+                this.b = color.b;
+            }
+
+            public RGB(Color32 color)
+            {
+                this.r = color.r / 255.0;
+                this.g = color.g / 255.0;
+                this.b = color.b / 255.0;
+            }
+
+            public RGB ToRGB()
+            {
+                return this;
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                this = rgb;
+            }
+
+            public Color ToColor(float a = 1f)
+            {
+                return new Color((float)r, (float)g, (float)b, a);
+            }
+
+            public Color32 ToColor32(byte a = 0xFF)
+            {
+                return new Color32((byte)(0xFF / r), (byte)(0xFF / g), (byte)(0xFF / b), a);
+            }
+        }
+
+        public struct RYB : IColorSpace
+        {
+            public double r;
+            public double y;
+            public double b;
+
+            public RYB(double r, double y, double b)
+            {
+                this.r = r;
+                this.y = y;
+                this.b = b;
+            }
+
+            public RGB ToRGB()
+            {
+                return RYBConverter.ToRGB(this);
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                this = RYBConverter.FromRGB(rgb);
+            }
+        }
+
+        public struct CMY : IColorSpace
+        {
+            public double c;
+            public double m;
+            public double y;
+
+            public CMY(double c, double m, double y)
+            {
+                this.c = c;
+                this.m = m;
+                this.y = y;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct CMYK : IColorSpace
+        {
+            public double c;
+            public double m;
+            public double y;
+            public double k;
+
+            public CMYK(double c, double m, double y, double k)
+            {
+                this.c = c;
+                this.m = m;
+                this.y = y;
+                this.k = k;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct HSL : IColorSpace
+        {
+            public double h;
+            public double s;
+            public double l;
+
+            public HSL(double h, double s, double l)
+            {
+                this.h = h;
+                this.s = s;
+                this.l = l;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct HSB : IColorSpace
+        {
+            public double h;
+            public double s;
+            public double b;
+
+            public HSB(double h, double s, double b)
+            {
+                this.h = h;
+                this.s = s;
+                this.b = b;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct HSV : IColorSpace
+        {
+            public double h;
+            public double s;
+            public double v;
+
+            public HSV(double h, double s, double v)
+            {
+                this.h = h;
+                this.s = s;
+                this.v = v;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct CIE_LAB : IColorSpace
+        {
+            public double l;
+            public double a;
+            public double b;
+
+            public CIE_LAB(double l, double a, double b)
+            {
+                this.l = l;
+                this.a = a;
+                this.b = b;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct Hunter_LAB : IColorSpace
+        {
+            public double l;
+            public double a;
+            public double b;
+
+            public Hunter_LAB(double l, double a, double b)
+            {
+                this.l = l;
+                this.a = a;
+                this.b = b;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct LHC : IColorSpace
+        {
+            public double l;
+            public double h;
+            public double c;
+
+            public LHC(double l, double h, double c)
+            {
+                this.l = l;
+                this.h = h;
+                this.c = c;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct LUV : IColorSpace
+        {
+            public double l;
+            public double u;
+            public double v;
+
+            public LUV(double l, double u, double v)
+            {
+                this.l = l;
+                this.u = u;
+                this.v = v;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct XYZ : IColorSpace
+        {
+            public double x;
+            public double y;
+            public double z;
+
+            public XYZ(double x, double y, double z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+
+            public RGB ToRGB()
+            {
+                return XYZConverter.ToRGB(this);
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                this = XYZConverter.FromRGB(rgb);
+            }
+        }
+
+        public struct YXY : IColorSpace
+        {
+            public double y1;
+            public double x;
+            public double y2;
+
+            public YXY(double y1, double x, double y2)
+            {
+                this.y1 = y1;
+                this.x = x;
+                this.y2 = y2;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public struct YUV : IColorSpace
+        {
+            public double y;
+            public double u;
+            public double v;
+
+            public YUV(double y, double u, double v)
+            {
+                this.y = y;
+                this.u = u;
+                this.v = v;
+            }
+
+            public RGB ToRGB()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void FromRGB(RGB rgb)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Utilities/Color/ColorSpaceConverters.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using static Expanse.Utilities.ColorSpace;
+
+namespace Expanse.Utilities
+{
+    public static class XYZConverter
+    {
+        public static RGB ToRGB(XYZ xyz)
+        {
+            double r = xyz.x * 3.2410 - xyz.y * 1.5374 - xyz.z * 0.4986;
+            double g = -xyz.x * 0.9692 + xyz.y * 1.8760 - xyz.z * 0.0416;
+            double b = xyz.x * 0.0556 - xyz.y * 0.2040 + xyz.z * 1.0570;
+
+            RGB rgb = new RGB();
+
+            return rgb;
+        }
+
+        public static XYZ FromRGB(RGB rgb)
+        {
+            double r = (rgb.r > 0.04045) ? Math.Pow((rgb.r + 0.055) / (1 + 0.055), 2.2)
+                : (rgb.r / 12.92);
+            double g = (rgb.g > 0.04045) ? Math.Pow((rgb.g + 0.055) / (1 + 0.055), 2.2)
+                : (rgb.g / 12.92);
+            double b = (rgb.b > 0.04045) ? Math.Pow((rgb.b + 0.055) / (1 + 0.055), 2.2)
+                : (rgb.b / 12.92);
+
+            XYZ xyz = new XYZ
+            {
+                x = rgb.r * 0.4124 + rgb.g * 0.3576 + rgb.b * 0.1805,
+                y = rgb.r * 0.2126 + rgb.g * 0.7152 + rgb.b * 0.0722,
+                z = rgb.r * 0.0193 + rgb.g * 0.1192 + rgb.b * 0.9505
+            };
+
+            return xyz;
+        }
+    }
+
+    public static class RYBConverter
+    {
+        public static RGB ToRGB(RYB ryb)
+        {
+            double r = ryb.r * 255;
+            double y = ryb.y * 255;
+            double b = ryb.b * 255;
+
+            double white = Math.Min(Math.Min(r, y), b);
+
+            r -= white;
+            y -= white;
+            b -= white;
+
+            double maxY = Math.Max(Math.Max(r, y), b);
+            double g = Math.Min(y, b);
+
+            y -= g;
+            b -= g;
+
+            if (b > 0 && g > 0)
+            {
+                b *= 2.0;
+                g *= 2.0;
+            }
+
+            r += y;
+            g += y;
+
+            double maxG = Math.Max(Math.Max(r, g), b);
+
+            if (maxG > 0)
+            {
+                double factor = maxY / maxG;
+
+                r *= factor;
+                g *= factor;
+                b *= factor;
+            }
+
+            r += white;
+            g += white;
+            b += white;
+
+            RGB rgb = new RGB
+            {
+                r = r / 255,
+                g = g / 255,
+                b = b / 255
+            };
+
+            return rgb;
+        }
+
+        public static RYB FromRGB(RGB rgb)
+        {
+            double r = rgb.r * 255;
+            double g = rgb.g * 255;
+            double b = rgb.b * 255;
+
+            double white = Math.Min(Math.Min(r, g), b);
+
+            r -= white;
+            g -= white;
+            b -= white;
+
+            double maxG = Math.Max(Math.Max(r, g), b);
+            double y = Math.Min(r, g);
+
+            r -= y;
+            g -= y;
+
+            if (b > 0 && g > 0)
+            {
+                b *= 0.5;
+                g *= 0.5;
+            }
+
+            y += g;
+            b += g;
+
+            double maxY = Math.Max(Math.Max(r, y), b);
+
+            if (maxY > 0)
+            {
+                double factor = maxG / maxY;
+
+                r *= factor;
+                y *= factor;
+                b *= factor;
+            }
+
+            r += white;
+            y += white;
+            b += white;
+
+            RYB ryb = new RYB
+            {
+                r = r / 255,
+                y = y / 255,
+                b = b / 255
+            };
+
+            return ryb;
+        }
+    }
+}
